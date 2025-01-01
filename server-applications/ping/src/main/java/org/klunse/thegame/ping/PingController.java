f45432b39cd8d9bf8adb6c2a3b639e39e1b34403
Nicolai Marck Ødum <nicolai@niro-it.dk>
2025-01-01 21:46:23 +0100

    Refactor project structure and update Gradle configurations

---
+++ server-applications/ping/src/main/java/org/klunse/thegame/ping/PingController.java
@@ -0,0 +1,14 @@
+package org.klunse.thegame.ping;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class PingController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Ping!";
+    }
+    
+}
